e5b26ee8729abd469a608c7b70d36e115d809d8f
den <andersonluizs3a@gmail.com>
2022-10-25 12:53:55 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main (String [] args) {
+        //classe Operadores.java
+            String nome = "GLEYSON";
+            int idade = 22;
+            double peso = 68.5;
+            char sexo = 'M';
+             
+
+            
+           System.out.print(peso);
+
+           System.out.print(idade);
+
+           System.out.print(sexo);
+           
+
+    }
+}
