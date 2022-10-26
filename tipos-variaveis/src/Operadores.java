aea7da8f2aec5434d76094a109b4012a36996ad5
den <andersonluizs3a@gmail.com>
2022-10-26 13:38:56 -0300

    operadores e metodos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,9 @@
 public class Operadores {
-    public static void main (String [] args) {
-        //classe Operadores.java
+    public static void main (String [] args) 
             String nome = "GLEYSON";
             int idade = 22;
             double peso = 68.5;
             char sexo = 'M';
-             
-
             
            System.out.print(peso);
 
